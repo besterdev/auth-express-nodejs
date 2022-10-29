7918ff5ff4802d158d1dcf2b5d75171bc3b5c467
Boy <thawatchai.krai@gmail.com>
2022-10-29 22:54:52 +0700

    ✨ Add route verified otp

--- routes/Users.ts
+++ routes/Users.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 import dayjs from 'dayjs'
+import _ from 'lodash'
+
 import * as fs from 'fs'
 import * as path from 'path'
 import * as handlebars from 'handlebars'
@@ -10,7 +12,11 @@ const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 const nodemailer = require('nodemailer')
 
-const sendOTPVerificationEmail = async (user: any) => {
+const verifyToken = require('../middlewares/Auth')
+
+type User = { id: number; email: any; password: string; verified: boolean }
+
+const sendOTPVerificationEmail = async (user: User) => {
   const filePath = path.join(__dirname, '../emails/verification.html')
   const source = fs.readFileSync(filePath, 'utf-8').toString()
   const template = handlebars.compile(source)
@@ -78,6 +84,63 @@ router.post('/signup', async (req: Request, res: Response) => {
 
     res.json({
       message: 'please verification otp email sent 📧',
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post('/verified', verifyToken, async (req: Request, res: Response) => {
+  try {
+    const { userId, otp } = req.body
+
+    if (!userId || !otp) {
+      res.status(401).json('Empty otp details are not allowed')
+      throw Error('Empty otp details are not allowed')
+    }
+
+    const userOTP = await prisma.userOTP.findUnique({
+      where: {
+        userId: userId,
+      },
+    })
+
+    if (_.size(userOTP) <= 0) {
+      res.status(401).json("Account record doesn't exist or has been verified already. Please sign up or login")
+      throw Error("Account record doesn't exist or has been verified already. Please sign up or login")
+    }
+
+    const expires = dayjs().isAfter(dayjs(userOTP?.expiresAt))
+
+    if (expires) {
+      res.status(401).json('Code has expired. Please request, again.')
+      throw new Error('Code has expired. Please request, again.')
+    }
+
+    const compareOTP = await bcrypt.compare(otp, userOTP?.otp)
+
+    if (!compareOTP) {
+      res.status(401).json('Invalid code passed. Check your index.')
+      throw new Error('Invalid code passed. Check your index.')
+    }
+
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        verified: true,
+      },
+    })
+
+    await prisma.userOTP.deleteMany({
+      where: {
+        userId: userId,
+      },
+    })
+
+    res.status(401).json({
+      message: 'verify otp success ✅',
     })
   } catch (error) {
     console.log(error)
